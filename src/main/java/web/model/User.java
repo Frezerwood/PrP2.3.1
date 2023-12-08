db283b897fbb9d00bff4b1a6bf75540f33de8f87
Aleksandr Lomakin <samarazoyl@gmail.com>
2023-12-08 13:24:33 +0400

    шiinstallation and configuration hibernate

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,2 +1,21 @@
-package web.model;public class User {
+package web.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String surname;
+
+    private int age;
 }
