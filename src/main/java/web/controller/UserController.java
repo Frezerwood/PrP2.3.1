b70f21f717f0730c01dc15e0a2ac413e34f75046
Aleksandr Lomakin <samarazoyl@gmail.com>
2023-12-09 21:00:29 +0400

    form added

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -23,5 +23,29 @@ public class UserController {
         return "list";
     }
 
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "user-form";
+    }
 
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("userId") Long id, Model model) {
+        User  user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String deleteUser(@RequestParam("userId") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/employees/list";
+    }
 }
